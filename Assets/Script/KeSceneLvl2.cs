a0485ec6430f102f264fc669b3200258ee60b20a
dzikrihf07 <dzikrihf@gmail.com>
2021-09-06 18:04:53 +0700

    Perbaikan Soud, Perbaikan soal level 2, perbaikan materi

---
+++ Assets/Script/KeSceneLvl2.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeSceneLvl2 : MonoBehaviour
+{
+    public void btPindah(){
+        Application.LoadLevel("SoalLvl2");
+    }
+}
